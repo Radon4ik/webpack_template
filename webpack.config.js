84b665e2b49dd56f28ce13800f4283d1e5d09e0f
Radon4ik <Loboda.Radimir@gmail.com>
2022-03-11 22:17:21 +0200

    edit files' ' '

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'targetFile.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'src'),
+        },
+        compress: true,
+        port: 9000,
+    },
+};
